d86612fbd6b34e9f672a35c38f2cfd2f00853b50
Pramesh10 <kcpramesh444@gmail.com>
2023-04-07 14:35:30 +0545

    Add project files.

---
+++ ChatApp/Hubs/HubUser.cs
@@ -0,0 +1,8 @@
+﻿namespace ChatApp.Hubs
+{
+    public class HubUser
+    {
+        public string UserIdentifier { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ ChatApp/MapperProfiles/AppProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using DataLayer;
+using ChatApp.Models;
+
+namespace ChatApp.MapperProfiles
+{
+    public class AppProfile : Profile
+    {
+        public AppProfile()
+        {
+            CreateMap<User, UserModel>()
+                .ReverseMap();
+
+            CreateMap<Group, GroupModel>()
+                .ReverseMap();
+        }
+    }
+}
